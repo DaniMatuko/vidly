23a3dffd06f89d06ba51de0ac557c6ea6c402f27
Dani Matuko <matuko305@gmail.com>
2020-08-23 23:34:35 +0300

    connection string

--- startup/db.js
+++ startup/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 module.exports = function() {
     // Connect to DB
-    mongoose.connect('mongodb://localhost/vidly', { useNewUrlParser: true, useUnifiedTopology: true })
+    mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true })
         .then(() => console.log('Connected to DB...'))
         .catch((err) => console.error('connection error: ', err.message));
 
